e28c8f0ca64274472f02f5cd715132bb4b43ab55
7doggy <seven.doggy@gmail.com>
2019-06-10 20:20:45 +0800

    update

--- src/main/java/com/lichun/rabbitmq/demo/hello/Receiver.java
+++ src/main/java/com/lichun/rabbitmq/demo/hello/Receiver.java
@@ -1,9 +1,27 @@
 package com.lichun.rabbitmq.demo.hello;
 
+import com.lichun.rabbitmq.demo.until.ConnectionUtil;
+import com.rabbitmq.client.Channel;
+import com.rabbitmq.client.Connection;
+import com.rabbitmq.client.DeliverCallback;
+
+
 public class Receiver {
     private final static String QUEUE_NAME = "testhello"; //队列名字
 
-    public static void main(String[] args) {
+    public static void main(String[] args) throws Exception {
+        Connection connection = ConnectionUtil.getConnection();
+        Channel channel = connection.createChannel();
+        channel.queueDeclare(QUEUE_NAME, false, false, false, null);
 
+        System.out.println(" [*] Waiting for message. To exit press CTRL+C ");
+
+        DeliverCallback deliverCallback = (consumerTag, delivery) -> {
+            String message = new String(delivery.getBody(), "UTF-8");
+            System.out.println(" [x] Received '" + message + "'");
+        };
+        channel.basicConsume(QUEUE_NAME, true, deliverCallback, consumerTag -> { });
+
+//        connection.close();
     }
 }
